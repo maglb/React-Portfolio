906750bf8251c19b031a2b7012211dd1f8f9f121
maglb <magali.lb21@gmail.com>
2024-03-27 17:54:49 -0600

    Database for project completed

---
+++ src/utils/Projects.js
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    id: 1,
+    title: "Fists of Forecast",
+    description: "description project here tktktktkt",
+    image: "../assets/FistsofForecast.png",
+    app: "https://verred01.github.io/Fists-of-Forecast/",
+    github: "https://github.com/Verred01/Fists-of-Forecast",
+  },
+  {
+    id: 2,
+    title: "The Green Hill",
+    description: "description project here tktktktkt",
+    image: "../assets/GreenHill.png",
+    app: "https://the-green-hill-ecommerce-admin-b26231d8324c.herokuapp.com/",
+    github:
+      "https://github.com/ryandarton/the-green-hill-ecommerce-admin-portal",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description: "description project here tktktktkt",
+    image: "",
+    app: "",
+    github: "",
+  },
+];
+
+export default projects;
