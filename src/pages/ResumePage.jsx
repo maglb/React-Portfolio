ed522b98a12eca62cff79c1e6397b1737e460c77
maglb <magali.lb21@gmail.com>
2024-03-28 17:58:41 -0600

    Resume page content and formatting

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,19 +1,63 @@
 // This is a static page mocking an "About Us" section for our fake user data
 export default function ResumePage() {
   return (
-    <section id="resume">
-      <div className="mydescription">
-        <h2>Resumee</h2>
-        <p id="mystory">
-          My name is Magali and I’m a graphic designer who’s currently studying
-          coding. I have a passion for typography and I enjoy helping brands
-          find their unique voice. When I'm not behind a computer solving design
-          problems, you can find me outside rock climbing, building my
-          problem-solving skills outside of the digital world.
-        </p>
+    <section id="about">
+      <div className="mydescription d-flex flex-column">
+        <h2>Resume</h2>
+
+        <div id="mystory" className="col">
+          <div className="skills">
+            <h4>Skills</h4>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>MySQL</li>
+              <li>NodeJS</li>
+              <li>ReactJS</li>
+              <li>MongoDB</li>
+              <li>GraphQL</li>
+            </ul>
+          </div>
+          <h4>Education</h4>
+          <div>
+            <h5>
+              <strong>Full-Stack Bootcamp Certification</strong> – April 2024
+            </h5>
+            <p className="location">
+              University of Utah, Salt Lake City, UT, United States
+            </p>
+          </div>
+          <div>
+            <h5>
+              <strong>Master Degree in Graphic Communication</strong> – June
+              2019
+            </h5>
+            <p className="location">
+              Haute école des arts du Rhin, Strasbourg, France
+            </p>
+          </div>
+          <div>
+            <h5>
+              <strong>Bachelor Degree in Graphic Communication</strong> – June
+              2017
+            </h5>
+            <p className="location">
+              Haute école des arts du Rhin, Strasbourg, France
+            </p>
+          </div>
+          <div>
+            <h5>
+              <strong>Bachelor Degree in Fine Art</strong> – June 2014
+            </h5>
+            <p className="location">
+              National School of Fine Art of Nice, Nice, France
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="myphoto">
-        <img src="../src/assets/myphoto.jpg" alt=""></img>
+      <div className="myphoto d-flex ">
+        <img src="../src/assets/resume.jpg" alt=""></img>
       </div>
     </section>
   );
