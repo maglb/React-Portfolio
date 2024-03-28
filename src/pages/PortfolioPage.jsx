3d94dcbc8d7c4549f142d9ad369ad723f256148d
maglb <magali.lb21@gmail.com>
2024-03-28 16:43:08 -0600

    Portfolio Page created

---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,11 @@
+import Portfolio from "../components/Portfolio"
+import "../style.css";
+
+export default function PortfolioPage() {
+  return  (
+  <section id="about">
+    <h2>Portfolio</h2>
+   <Portfolio />
+    </section>
+    );
+}
