2f8e6676aa3d09e5347dbbfef4ecb41d54a8ffbe
maglb <magali.lb21@gmail.com>
2024-03-28 16:43:18 -0600

    ResumePage created

---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,20 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function ResumePage() {
+  return (
+    <section id="resume">
+      <div className="mydescription">
+        <h2>Hi,</h2>
+        <p id="mystory">
+          My name is Magali and I’m a graphic designer who’s currently studying
+          coding. I have a passion for typography and I enjoy helping brands
+          find their unique voice. When I'm not behind a computer solving design
+          problems, you can find me outside rock climbing, building my
+          problem-solving skills outside of the digital world.
+        </p>
+      </div>
+      <div className="myphoto">
+        <img src="../src/assets/myphoto.jpg" alt=""></img>
+      </div>
+    </section>
+  );
+}
