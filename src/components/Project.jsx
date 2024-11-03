5355c59d1d64ef2578bf1d47c7740865cc8eeda8
Clear Strategy Partners <contact@cspfirm.com>
2024-11-03 12:09:36 -0700

    Technologies section added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,19 @@
 export default function Project(props) {
     return (
-      <div class=" mb-3 col-lg-9 mx-auto" id="portfolio">
-        <img src={props.image} class="card-img-top" alt="..."></img>
+      <div class=" mb-3 col-lg-9 mx-auto" className="portfolio">
+        <a target="_blank" href={props.app} role="button">
+          <img src={props.image} class="card-img-top" alt="..."></img>
+        </a>
         <div class="card-body">
           <h3 class="card-title">{props.title}</h3>
-          <p class="card-text description">
-            {props.description}
-          </p>
+          <p class="card-text description">{props.description}</p>
+          <p class="card-text technology">{props.technology}</p>
           <a class="btn" target="_blank" href={props.app} role="button">
             See Application
           </a>
-          <a class="btn" target="_blank" href={props.github} role="button">
+          {/* <a class="btn" target="_blank" href={props.github} role="button">
             Github
-          </a>
+          </a> */}
         </div>
       </div>
     );
