64db32af8ad62008c3a5c4b0a75790bcd1a58c79
maglb <magali.lb21@gmail.com>
2024-11-21 16:25:24 -0700

    Removed icons from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,7 @@ import "../style.css";
 export default function Footer() {
   return (
     <footer>
-      <div className="icon-container d-flex col justify-content-center">
-        <a className="icon" href="https://github.com/maglb" target="_blank">
-          <i className="fa fa-github"></i>
-        </a>
-        <a
-          className="icon"
-          href="https://www.linkedin.com/in/magali-lebon"
-          target="_blank"
-        >
-          <i className="fa fa-linkedin-square"></i>
-        </a>
-      </div>
+<p> Magali Lebon 2024</p>
     </footer>
   );
 }
