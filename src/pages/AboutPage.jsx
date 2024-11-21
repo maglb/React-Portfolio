07af6981011c86c6003a35880df63a96139bb4a2
maglb <magali.lb21@gmail.com>
2024-11-21 16:26:13 -0700

    updated copy for about section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,15 +7,21 @@ export default function AboutPage() {
       <div className="mydescription">
         <h2>Hi,</h2>
         <p id="mystory">
-          My name is Magali and I’m a graphic designer who’s currently studying
-          coding. I have a passion for typography and I enjoy helping brands
-          find their unique voice. When I'm not behind a computer solving design
-          problems, you can find me outside rock climbing, building my
-          problem-solving skills outside of the digital world.
+          With my formal training as a full-stack software engineer and my
+          background in graphic design, I bring a unique blend of creativity and
+          technical expertise to my work. I excel at crafting
+          user-centered digital experiences that seamlessly integrate aesthetics
+          and functionality, ensuring that every application not only performs
+          well but also delights users. <br></br>
+          <br></br>
+          Outside of coding, I enjoy{" "}
+          <a href="https://magalilebon.squarespace.com/" target="_blank">
+            graphic design
+          </a> and rock climbing.
         </p>
       </div>
       <div className="myphoto">
-        <img src={myPhoto} alt=""></img>
+        <img src={myPhoto} alt="Portrait of Magali Lebon"></img>
       </div>
     </section>
   );
